130ab6719a0fa5f5e671001222c6ca5e8998ffff
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-29 19:53:09 +0600

    1st practice done need more

--- js/main.js
+++ js/main.js
@@ -7,3 +7,39 @@ let h1 =document.querySelector('h1')
 
 
 
+
+
+let handlebutton =()=>{
+   
+    
+    if(input.value == '')
+    {
+        h2.innerHTML="inpuet any number what ever you want"
+    }
+
+
+    else if(input.value > 0){
+            
+
+    let i =0
+
+
+    let count = ()=>{
+
+    i++
+
+ if(input.value == i){
+    clearInterval(out)
+ }
+
+ h1.innerHTML=i
+    }
+
+
+
+
+     let out =  setInterval(count,100)
+
+
+    }
+}
